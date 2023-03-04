a6360fc84f9462d065065f64aaae30ed1f9cc8bc
Koushik Dey <koushik.dey@live.com>
2023-03-05 01:23:59 +0200

    added eventing system

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,14 @@ import { User } from './models/User';
 
 const user = new User({ name: 'rajon', age: 102 });
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.on('change', () => {
+  console.log('change # 1');
+ });
+user.on('change', () => {
+  console.log('change # 2');
+ });
+user.on('save', () => {
+  console.log('save was triggered');
+ });
+
+ user.trigger('change');
