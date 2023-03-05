45cc21048b6393d5928e9d84ec59c3cacd443ff2
Koushik Dey <koushik.dey@live.com>
2023-03-05 13:12:22 +0200

    implemented json-server and REST

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,5 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'rajon', age: 102 });
+const user = new User({ name: 'new record', age: 0 });
 
-user.on('change', () => {
-  console.log('change # 1');
- });
-user.on('change', () => {
-  console.log('change # 2');
- });
-user.on('save', () => {
-  console.log('save was triggered');
- });
-
- user.trigger('change');
+user.save();
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,15 +1,14 @@
+import axios, { AxiosResponse } from 'axios';
+
 interface UserProps {
   // ? used for optional
   // for updating any property individually
+  id?: number;
   name?: string;
   age?: number;
 }
-type Callback = () => void;
-
 export class User {
-  events: { [key: string]: Callback[] } = {};
-
-  constructor(private data: UserProps) { }
+  constructor(private data: UserProps) {}
 
   get(propName: string): (number | string) {
     return this.data[propName];
@@ -19,21 +18,24 @@ export class User {
     Object.assign(this.data, update);
   }
 
-  on(eventName: string, callback: Callback) {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
+  fetch(): void {
+    const id = this.get('id');
+
+    axios
+      .get(`http://localhost:3000/users/${id}`)
+      .then((response: AxiosResponse): void => {
+        this.set(response.data);
+      });
   }
 
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
-    if (!handlers || handlers.length === 0) {
-      return;
-    }
+  save(): void {
+    const id = this.get('id');
 
-    handlers.forEach(callback => {
-      callback();
-    });
+    if (id) {
+      axios.put(`http://localhost:3000/users/${id}`, this.data);
+    } else {
+      axios.post('http://localhost:3000/users/', this.data);
+    }
   }
 }
 
